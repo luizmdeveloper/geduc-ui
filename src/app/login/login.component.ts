286d0947dbfadb0da270246a1904e0996975a691
luizmarioinfo <luizmario@inforio.com.br>
2017-11-06 12:02:09 -0300

    Ajustando chamada do login, iniciando recuperar senha

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,14 @@
 import { FormControl } from '@angular/forms';
 import { Component } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
-import { Md5 } from 'ts-md5/dist/md5';
 
 import { ToastyService } from 'ng2-toasty';
+import { Message } from 'primeng/components/common/api';
+import { MessageService } from 'primeng/components/common/messageservice';
 
-import 'rxjs/add/operator/map';
 import { environment } from './../../environments/environment';
+import { Md5 } from 'ts-md5/dist/md5';
+import 'rxjs/add/operator/toPromise';
 
 export class Parametro {
   codigo: number;
@@ -33,23 +34,28 @@ export class Boletim {
 })
 export class LoginComponent {
   private _Url: string;
-  private data: any = null;
+  private msgs: Message[] = [];
 
-  constructor(private _http: Http) {}
+  constructor(private _http: Http,
+              private messageService: MessageService) {
+    this.msgs.push({severity: 'error', summary: 'Error Message', detail: 'Ponha essa porra melhor'});
+  }
 
-  logar(login: string, senha: string) {
+  logar(login: string, senha: string): Promise<any> {
     this._Url = `${environment.apiUrl}` + 'getUsuario/' + `${environment.empresa}` + '/' + `${environment.empresa}` + '/';
 
-    this._Url = this._Url + login + '/' + Md5.hashStr(senha);
+    this._Url = this._Url + login + '/' + Md5.hashStr(senha).toString().toUpperCase();
 
     return this._http.get(this._Url)
-            .map((res: Response) => res.json())
-            .subscribe(data => {
-                this.data = data;
-                if (this.data.empty) {
-                  // this.toasty.error('usuário ou senha inválido');
-                }
-            });
+          .toPromise()
+          .then(response => {
+            if (response.json().result[0].is) {
+              console.log('Usuário não cadastrado');
+            }else {
+              console.log(response.json().result[0]);
+            }
+          })
+          .catch(err => console.log(err));
   }
 
 }
